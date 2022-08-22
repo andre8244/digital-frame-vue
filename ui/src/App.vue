f5444457ccb2307de7883748bacc3a8037d436a7
Andrea Leardini <andre8244@gmail.com>
2022-08-22 16:03:47 +0200

    ui: first scaffold

---
+++ ui/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="px-3">
+    <nav class="my-3 flex justify-between">
+      <RouterLink to="/" class="font-bold">Digital Frame</RouterLink>
+      <RouterLink
+        to="/settings"
+        class="inline-block rounded-lg px-5 py-2 bg-gray-600 tracking-wider font-semibold"
+      >
+        Settings
+      </RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<style></style>
